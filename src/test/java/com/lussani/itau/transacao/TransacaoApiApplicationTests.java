2d94160014e052ec1eb5bc6227b72bae09151b3f
Pedro Lussani <p.henriquelussani@gmail.com>
2026-03-29 19:10:51 -0300

    chore: initializes spring boot project

---
+++ src/test/java/com/lussani/itau/transacao/TransacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lussani.itau.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

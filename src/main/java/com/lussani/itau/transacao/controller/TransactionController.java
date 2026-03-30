4d6aa79f6ce9b4f6005de6ac948c65359eb5ff33
Pedro Lussani <p.henriquelussani@gmail.com>
2026-03-29 21:22:14 -0300

    feat: add DELETE /transacao

--- src/main/java/com/lussani/itau/transacao/controller/TransactionController.java
+++ src/main/java/com/lussani/itau/transacao/controller/TransactionController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +26,11 @@ public class TransactionController {
     public ResponseEntity<Void> add(@Valid @RequestBody TransactionRequest transaction){
         transactionService.addTransaction(transaction);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(){
+        transactionService.clearTransactions();
+        return ResponseEntity.ok().build();
     }
 }
